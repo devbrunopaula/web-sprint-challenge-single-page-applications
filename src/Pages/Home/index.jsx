3388771dfacba1978c17531d81e9ab052511b138
Bruno Paula <brunopaula@dryserv.com>
2020-07-17 21:37:41 -0400

    Inital Commit

---
+++ src/Pages/Home/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Slider from '../../components/Carousel/Slider'
+function Home() {
+    return (
+        <div>
+            <Slider/>
+            <h1>Home</h1>
+        </div>
+    )
+}
+
+export default Home
